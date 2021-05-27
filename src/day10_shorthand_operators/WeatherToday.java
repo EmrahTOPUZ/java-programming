6b77536136b80b4cc9c52591c0f14911073fb6d0
EmrahTOPUZ <emrtpz@gmail.com>
2021-05-27 19:59:28 -0400

    added Address class

---
+++ src/day10_shorthand_operators/WeatherToday.java
@@ -0,0 +1,18 @@
+package day10_shorthand_operators;
+
+import java.util.Scanner;
+
+public class WeatherToday {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How is the weather today?");
+
+        //String weather = "sunny";
+        String weather = scan.nextLine();
+
+        System.out.println("Weather is " + weather);
+
+
+    }
+}
