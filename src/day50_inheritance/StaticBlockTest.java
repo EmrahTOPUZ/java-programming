3f516853a5965602bb80ed9b302c249d168b926e
EmrahTOPUZ <emrtpz@gmail.com>
2021-06-05 11:43:58 -0400

    Day 50 start what is going on

--- src/day50_inheritance/StaticBlockTest.java
+++ src/day50_inheritance/StaticBlockTest.java
@@ -4,5 +4,7 @@ public class StaticBlockTest {
     public static void main(String[] args) {
         StaticBlockDemo st1 = new StaticBlockDemo();
         StaticBlockDemo st2 = new StaticBlockDemo();
+        StaticBlockDemo st3 = new StaticBlockDemo();
+        System.out.println(StaticBlockDemo.num);
     }
 }
