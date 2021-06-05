c3fe06fcae7700499791466e70fa72a93077b31c
EmrahTOPUZ <emrtpz@gmail.com>
2021-06-05 10:40:36 -0400

    Day49 1234 bank account

---
+++ src/day49_static/BankAccountTest.java
@@ -0,0 +1,16 @@
+package day49_static;
+
+public class BankAccountTest {
+    public static void main(String[] args) {
+
+        BankAccount husband = new BankAccount();
+        husband.user = "husband";
+        husband.showBalance();
+        husband.spend(100.0);
+        husband.showBalance();
+
+        BankAccount wife = new BankAccount();
+        wife.user = "wife";
+        wife.showBalance();
+    }
+}
