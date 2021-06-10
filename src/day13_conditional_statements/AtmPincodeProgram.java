d4df162a5c3fec94ca2533f0cd46c3d0e0d313c9
EmrahTOPUZ <emrtpz@gmail.com>
2021-06-10 11:46:09 -0400

    if else statement is continues

---
+++ src/day13_conditional_statements/AtmPincodeProgram.java
@@ -0,0 +1,26 @@
+package day13_conditional_statements;
+
+import java.util.Scanner;
+
+public class AtmPincodeProgram {
+    public static void main(String[] args) {
+        System.out.println("###### WELCOME TO CHASE ######");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("ENTER YOUR PINCODE");
+        int secretPincode = 1234;
+        int inputPincode = scan.nextInt();
+
+
+
+        if (secretPincode == inputPincode) {
+            System.out.println("Your Pin is correct");
+            System.out.println("WELCOME TO YOUR ACCOUNT");
+        } else {
+            System.out.println("Your Pin is incorrect");
+            System.out.println("Please try again");
+        }
+
+
+    }
+}
