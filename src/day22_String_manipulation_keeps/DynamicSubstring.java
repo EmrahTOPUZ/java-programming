ed27569be4b45c8bf16689b68bcb747f0d158eba
EmrahTOPUZ <emrtpz@gmail.com>
2021-06-20 16:52:23 -0400

    String manipulation .substring

---
+++ src/day22_String_manipulation_keeps/DynamicSubstring.java
@@ -0,0 +1,22 @@
+package day22_String_manipulation_keeps;
+
+public class DynamicSubstring {
+    public static void main(String[] args) {
+        String result = "result count:12345";
+        System.out.println(result.substring(13,18));
+        System.out.println(result.substring(13));
+
+        System.out.println(result.indexOf(":"));
+        int idxOfColon = result.indexOf(":");
+        System.out.println(result.substring(idxOfColon+1));
+
+        System.out.println(result.substring(result.indexOf(":") +1));
+
+        String today = "i learned [java] today";
+        int start = today.indexOf("[");
+        int end = today.indexOf("]");
+        System.out.println(today.substring(start +1 , end));
+
+        System.out.println(today.substring(today.indexOf("[") +1, today.indexOf("]")));
+    }
+}
