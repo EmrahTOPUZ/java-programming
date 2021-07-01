d8f0a52540cbe4e7215d55e31a1e0fe20ce51c92
EmrahTOPUZ <emrtpz@gmail.com>
2021-07-01 12:53:18 -0400

    Intro to Arrays

---
+++ src/day29_nestedloop_arrays/ArraysPractice.java
@@ -0,0 +1,21 @@
+package day29_nestedloop_arrays;
+
+public class ArraysPractice {
+    public static void main(String[] args) {
+
+        int [ ] nums = new int [2];
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+
+        int [] counters = {12, 23, (int)34.45, 45, 67};
+
+        double [] prices = { 12.22, 34.07, 356.6};
+
+        String [] cities = {"New York", "Miami" , "Los Angeles", "Zonguldak"};
+
+        System.out.println(counters[4]);
+        System.out.println(prices[1]);
+        System.out.println(cities[3]);
+
+    }
+}
