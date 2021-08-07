aee5205b3441d2802f2db7e9d8df13558ecedf11
EmrahTOPUZ <emrtpz@gmail.com>
2021-08-06 20:00:04 -0400

    display USA flag with for loop and created method

---
+++ src/day35_methods_with_param/VoidMethods.java
@@ -0,0 +1,43 @@
+package day35_methods_with_param;
+
+public class VoidMethods {
+    public static void main(String[] args) {
+        printAtoZ();
+        printAtoZ();
+
+        for (int i = 0; i < 10; i++) {
+            printAtoZ();
+        }
+
+        displayUsaFlag();
+
+        //System.out.println("---------------FLAG OF USA--------------");
+        displayUsaFlag();
+
+    }
+
+    /**
+     * method name: printAtoZ
+     * desc: print English alphabet in all uppercase
+     */
+
+    public static void printAtoZ() {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void displayUsaFlag() {
+        System.out.println("---------------FLAG OF USA--------------");
+        String p1 = "* * * * * * ==================================\n * * * * *  ==================================";
+        String p2 = "==============================================";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(p1);
+        }
+        System.out.println("* * * * * * ==================================");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(p2);
+        }
+    }
+}
